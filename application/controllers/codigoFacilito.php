20b66525e4c82d797c68f4fd9ffe25e77dc08faa
igor <igor_pe@icloud.com>
2014-01-25 11:27:55 +0100

    rueue

---
+++ application/controllers/codigoFacilito.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class CodigoFacilito extends CI_Controller {
+	function __construct(){
+		parent:: __construct();
+	}
+	#sdfasd
+	function index(){
+		$this->load->library('menu',array('Inicio','Contacto','Cursos'));
+		$data['mi_menu'] = $this->menu->construirMenu();
+		$this->load->view('codigoFacilito/bienvenido',$data); 	
+		$this->load->view('codigoFacilito/header');	
+	}
+
+	function holaMundo(){
+		$this->load->view('codigoFacilito/bienvenido');	
+		$this->load->view('codigoFacilito/header');	
+	}
+}
+
+?>
